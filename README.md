3ab9aea0a9d27729f9e175fd22e28335fea49557
sonichy <sonichy@163.com>
2019-01-22 20:36:30 +0800

    add shortcut keys

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ And it will be more easier to get HLS url with [VideoGrab](https://github.com/so
 
 ![alt](preview.png)  
 
+|Key|Function|
+|:-:|:-:|
+|←|Seek Backward|
+|→|Seek Forward|
+|↑|Volume Up|
+|↓|Volume Down|
+|M|Mute|
+|F|Enter Fullscreen|
+|Space|Pause or Play|
+|Mouse Left Click|Pause or Play|
+
 ## Reference:  
 [Native HLS Playback](https://chrome.google.com/webstore/detail/native-hls-playback/emnphkkblegpebimobpbekeedfgemhof)  
 ## Lib:  
--- player.js
+++ player.js
@@ -8,3 +8,36 @@ if(Hls.isSupported()) {
 		video.play();
 	});
 }
+
+document.onkeydown = function(event){
+	//console.log(event.keyCode);
+	if(event.keyCode == 32){
+		if(video.paused) {
+			video.play();
+		}else{
+			video.pause();
+		}
+	}else if(event.keyCode == 39){
+		video.currentTime += 5;
+	}else if(event.keyCode == 37){
+		video.currentTime -= 5;
+	}else if(event.keyCode == 38){
+		if(video.volume < 1)
+			video.volume += 0.1;
+	}else if(event.keyCode == 40){
+		if(video.volume > 0)
+			video.volume -= 0.1;
+	}else if(event.keyCode == 77){		
+		video.muted = !video.muted;
+	}else if(event.keyCode == 70){		
+		video.requestFullscreen();
+	}
+};
+
+video.onclick = function(){
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+}
