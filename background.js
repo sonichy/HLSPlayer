291c4af4b2445e53c20902b28562ee81feefdb0a
sonichy <sonichy@163.com>
2019-01-28 23:42:55 +0800

    Extention click open player page to fill url manually

--- background.js
+++ background.js
@@ -2,10 +2,14 @@ chrome.webRequest.onBeforeRequest.addListener(
 	function(info) {
 		console.log(info);
 		if (info.url.endsWith(".m3u8")) {
-			var playerUrl = chrome.runtime.getURL('player.html') + "#" + info.url
+			var playerUrl = chrome.runtime.getURL('player.html') + "#" + info.url;
 			return { redirectUrl:  playerUrl }      
 		}
 	},
 	{urls: ["*://*/*.m3u8*"], types:["main_frame"]},
 	["blocking"]
 );
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+	chrome.tabs.create({ 'url': chrome.extension.getURL('player.html') + '#' }, function(tab){ });
+});
